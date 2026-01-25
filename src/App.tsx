0b40737c544586c05ccf859bc2015765eb239c63
Miguel Salvador <miguel_f_r_salvador@hotmail.com>
2026-01-25 08:42:42 +0000

    feat: installed tanstack router, fixing bugs

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
+import { Router } from "@tanstack/react-router";
+// for type autocomplete
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof Router
+  }
+}
+
 
 export default function App() {
   return <MantineProvider theme={theme}>App</MantineProvider>;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { TanStackRouterDevtoolsInProd } from '@tanstack/react-router-devtools'
+
+
+const rootRoute = createRootRoute({
+  component: () => (
+    <>
+      <Outlet />
+      <TanStackRouterDevtools />
+    </>
+  ),
+})
+
+const routeTree = rootRoute.addChildren([
+  // ... other routes
+])
+
+const router = createRouter({
+  routeTree,
+})
+
+function App() {
+  return <RouterProvider router={router} />
+}
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
